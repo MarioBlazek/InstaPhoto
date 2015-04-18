fca5028c2e6409d00d3ff087d3322c960785ab9b
Marek <blazek.mario@gmail.com>
2015-04-18 10:34:10 +0200

    Initial Commit

---
+++ InstaPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaPhoto
+//
+//  Created by Marek on 18/04/15.
+//  Copyright (c) 2015 Marek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
